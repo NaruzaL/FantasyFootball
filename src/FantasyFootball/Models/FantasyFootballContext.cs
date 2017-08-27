adf3a0b2d8b55a5cb83b8191ad1f203e3bf93ebb
Pete Lazuran <pdlazuran@gmail.com>
2017-08-27 12:24:01 -0700

    setup skeleton of app w/ user auth

---
+++ src/FantasyFootball/Models/FantasyFootballContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace FantasyFootball.Models
+{
+    public class FantasyFootballContext
+    {
+        public FantasyFootballContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        //public virtual DbSet<Location> Locations { get; set; }
+        //public virtual DbSet<Experience> Experiences { get; set; }
+        //public virtual DbSet<People> People { get; set; }
+        //public virtual DbSet<Suggestion> Suggestions { get; set; }
+        public TravelBlogContext() { }
+    }
+}
