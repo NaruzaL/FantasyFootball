7123c2522c16d07c9e2e46fb19097380bf2dbf9f
Pete <pdlazuran@gmail.com>
2017-08-29 16:11:01 -0700

    work on fixing link back to Team/Index from Player/Index

--- src/FantasyFootball/Controllers/PlayerController.cs
+++ src/FantasyFootball/Controllers/PlayerController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using FantasyFootball.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 namespace FantasyFootball.Controllers
 {
-    public class PlayerController : Controller
+    public class Playercontroller : Controller
     {
-        private FantasyFootballContext db = new FantasyFootballContext();
-        public IActionResult Index()
+        private readonly FantasyFootballContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public Playercontroller(UserManager<ApplicationUser> userManager, FantasyFootballContext db)
         {
-            //db.Teams.FirstOrDefault()
-            return View(db.Players.ToList());
+            _userManager = userManager;
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            ViewBag.TeamId = _db.Teams.Where(x => x.User.Id == currentUser.Id);
+            return View(_db.Players.ToList());
         }
     }
 }
