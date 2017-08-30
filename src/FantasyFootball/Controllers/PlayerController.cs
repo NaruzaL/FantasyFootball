62e6f1d39cfdb4660229235c47fe3d41db1950e3
Pete <pdlazuran@gmail.com>
2017-08-30 11:39:41 -0700

    add AJAX to player index to add player to team

--- src/FantasyFootball/Controllers/PlayerController.cs
+++ src/FantasyFootball/Controllers/PlayerController.cs
@@ -13,19 +13,31 @@ namespace FantasyFootball.Controllers
 {
     public class Playercontroller : Controller
     {
-        private readonly FantasyFootballContext _db;
-        private readonly UserManager<ApplicationUser> _userManager;
-        
-        public Playercontroller(UserManager<ApplicationUser> userManager, FantasyFootballContext db)
-        {
-            _userManager = userManager;
-            _db = db;
-        }
+        private FantasyFootballContext db = new FantasyFootballContext();   
         public IActionResult Index(int id)
         {
-            var userTeam = _db.Teams.FirstOrDefault(team => team.TeamId == id);
+            var userTeam = db.Teams.FirstOrDefault(team => team.TeamId == id);
             ViewBag.TeamId = userTeam.TeamId;
-            return View(_db.Players.ToList());
+            return View(db.Players.ToList());
+        }
+        //[HttpPost]
+        //public IActionResult AddPlayer(int id, int tId)
+        //{
+        //    var thisPlayer = db.Players.FirstOrDefault(p => p.PlayerId == id);
+        //    var userTeam = db.Teams.FirstOrDefault(team => team.TeamId == tId);
+        //    ViewBag.TeamId = userTeam.TeamId;
+        //    return View(thisPlayer);
+        //}
+
+        [HttpPost]
+        public IActionResult AddPlayer(int id, int tid)
+        {
+            var thisPlayer = db.Players.FirstOrDefault(p => p.PlayerId == id);
+            thisPlayer.Team = db.Teams.FirstOrDefault(team => team.TeamId == tid);
+            thisPlayer.FreeAgent = false;
+            db.Entry(thisPlayer).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json(db.Players.ToList());
         }
     }
 }
